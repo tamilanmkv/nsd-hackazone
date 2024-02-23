f989713f0f636145ba6c01cef93c62f7cd56e8f4
Jijo James <jja@financial.com>
2024-02-23 16:27:04 +0530

    Add 1st challenge

---
+++ 1. buggy-product/Challenge.js
@@ -0,0 +1,9 @@
+// versions would be a 1D array with either '0' or '1' as its elements.
+// '0' indicates the version is bug-free and '1' indicates the version is buggy.
+// (Ex - For input [0, 0, 1, 1, 1], the bug was introduced in version 2 and the function should return 1)
+
+function lastBugFreeVersion(versions) {
+  // Your implementation here
+}
+
+lastBugFreeVersion([0, 0, 1, 1, 1]);
