ef16803ca47f77817f60ee61c02cfcb1bbee60a7
vasu <vasu@voxxup.com>
2024-03-02 12:01:42 +0530

    buggy product complete

--- 2. cleaning-robot/javascript/challenge.js
+++ 2. cleaning-robot/javascript/challenge.js
@@ -1,6 +1,6 @@
 // Robot controller implementation
 
-import { robotControl, Direction } from './robot-api.js';
+import { robotControl, Direction } from "./robot-api.js";
 
 const rc = robotControl("../resources/room-layout-1.txt");
 const api = rc.robotApi;
@@ -16,7 +16,80 @@ rc.evaluateResult();
 // api.isBarrierAhead(); // true -> barrier ahead
 // api.getPosition(); // returns the current robot position, e.g. { x: 1, y: 2 }
 // api.getPositionAhead(); // returns the position in front of the robot, doesn't check if a barrier
+// //write the code to clean the room
+
+function checkTurnRight(api) {
+  while (true) {
+    if (!api.isBarrierAhead()) {
+      api.turnRight();
+      if (api.isBarrierAhead()) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+    return false;
+  }
+}
+
+function checkTurnLeft(api) {
+  while (true) {
+    if (api.isBarrierAhead()) {
+      api.turnLeft();
+    }
+    return false;
+  }
+}
+
+function checkMove(api) {
+  while (true) {
+    if (!api.isBarrierAhead()) {
+      api.move();
+      if (api.isBarrierAhead()) {
+        console.log(api.getPosition());
+        break;
+      }
+    }
+    break;
+  }
+}
 
 function cleanRooms(api) {
-    // TODO add code here
+  while (true) {
+    if (!api.isBarrierAhead()) {
+      checkMove(api);
+    }
+    if (checkTurnRight(api)) {
+      if (!api.isBarrierAhead()) {
+        checkMove(api);
+      }
+    } else if (checkTurnLeft(api)) {
+      if (!api.isBarrierAhead()) {
+        checkMove(api);
+      }
+    }
+  }
 }
+
+function testCondtion(api) {
+  api.move(); // move robot one step forward
+  api.turnLeft(); // turn robot 90 degrees to the left
+  api.turnLeft(); // turn robot 90 degrees to the left
+  // api.turnRight(); // turn robot 90 degrees to the right
+  api.move(); // move robot one step forward
+  api.move(); // move robot one step forward
+  api.move(); // move robot one step forward
+  api.move(); // move robot one step forward
+  console.log(api.isBarrierAhead()); // true -> barrier ahead, api.isBarrierAhead();
+  const position = api.getPosition();
+  console.log(position);
+}
+
+// without any movie position is { x: 8, y: 0 }
+/* 
+XXX....R 
+XXX.....
+XXX.....
+........
+........
+*/
