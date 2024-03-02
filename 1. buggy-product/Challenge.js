33891a40b6f37fae63b34df7a9089cbc34f46379
vasu <vasu@voxxup.com>
2024-03-02 10:52:39 +0530

    buggy product complete

--- 1. buggy-product/Challenge.js
+++ 1. buggy-product/Challenge.js
@@ -1,9 +1,23 @@
-// versions would be a 1D array with either '0' or '1' as its elements.
-// '0' indicates the version is bug-free and '1' indicates the version is buggy.
-// (Ex - For input [0, 0, 1, 1, 1], the bug was introduced in version 2 and the function should return 1)
+// case 1 : first find the last bug free version and store the index
+// case 2 : then find the previous bug free version
+// example : [0, 0, 1, 1, 1]
+// case 3 : what if contain next bug free version after previous is buggy version
+// example : [0, 0, 1, 0, 1]
+// case 4 : what if there is no previous bug free version return null
+// example : [1, 1, 1, 1, 1]
+
+
+
 
 function lastBugFreeVersion(versions) {
-  // Your implementation here
+  const lastBugFreeVersion = versions.filter((x, i) =>
+    x === 0 ? i : null,
+  ).length;
+
+  return versions.filter((x) => x === 1 && lastBugFreeVersion).length;
 }
 
+// versions would be a 1D array with either '0' or '1' as its elements.
+// lastBugFreeVersion([0, 0, 1, 1, 1]);
 lastBugFreeVersion([0, 0, 1, 1, 1]);
+
